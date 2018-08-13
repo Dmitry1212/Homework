a7bdc5989b443d2d6383331b97283fe4b5f980df
Dmitry1212 <shamaev.dmitry@yandex.ru>
2018-08-13 19:34:15 +0300

    Добавление слуаштеля события

--- js2_feadback.js
+++ js2_feadback.js
@@ -1,3 +1,17 @@
 //console.log('hello');
 "use strict";
 
+//var sendButton = document.getElementById('send');
+//console.log(sendButton);
+
+send.addEventListener('click', checkForm());
+//document.getElementById('send').addEventListener('click', checkForm());
+
+function checkForm() {
+    console.log('hello');
+    var inputName = document.getElementById('name');
+    // var pattern = /^\[a-zA-ZА-Яа-пр-я]+$/;
+    // console.log(pattern.test)
+    //console.log(inputName);
+
+}
